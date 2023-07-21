dd1b29a8f9be82cf44b102b25bfa284ad123adac
tycyi <168303583@qq.com>
2023-07-21 18:29:05 +0800

    Site updated: 2023-07-21 18:29:05

--- js/light-dark-switch.js
+++ js/light-dark-switch.js
@@ -3,11 +3,11 @@ let themeFunc = async function() {
     let root = document.documentElement;
     btn.addEventListener('click', e => {
         if (root.getAttribute('theme') == 'light') {
+            root.setAttribute('theme', 'dar');
+            localStorage.setItem('theme', 'dar');
+        } else {
             root.setAttribute('theme', 'light');
             localStorage.setItem('theme', 'light');
-        } else {
-            root.setAttribute('theme', 'dark');
-            localStorage.setItem('theme', 'dark');
         }
     });
 };
