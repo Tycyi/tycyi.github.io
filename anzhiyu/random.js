2d2bf81418f30c89365a047ea18f645e1a35b1b1
tycyi <168303583@qq.com>
2023-07-23 03:36:31 +0800

    Site updated: 2023-07-23 03:36:31

--- anzhiyu/random.js
+++
@@ -1,1 +0,0 @@
-var posts=["2023/07/20/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
---
+++ js/random.js
@@ -0,0 +1,17 @@
+var posts=["2023/07/20/hello-world/"];
+                    function toRandomPost() {
+                        window.open('/'+posts[Math.floor(Math.random() * posts.length)],"_self");
+                        new Vue({
+                            data: function () {
+                                this.$notify({
+                                    title: "跳转中",
+                                    message: "前往随机文章",
+                                    position: 'top-left',
+                                    offset: 50,
+                                    showClose: true,
+                                    type: "success",
+                                    duration: 4000
+                                });
+                            }
+                        });
+                    };
